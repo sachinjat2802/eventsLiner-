36b01fd6fc33d98a32b2dfb4fd4abb461916a465
sachin jat <53407215+sachinjat2802@users.noreply.github.com>
2022-12-14 12:43:20 +0530

    added customers in admin part

--- api/public/v1/index.js
+++ api/public/v1/index.js
@@ -1,27 +1,10 @@
 import { Router } from "express";
-
-
 import AssetUploadRoutes from "./AssetsModule/assets.router.js";
 import adminUserRoutes from "./AdminUserModule/adminUserAuth.router.js";
 
-import userRoutes from "./userModule/user.route.js";
-
-
-
 const router = Router();
-
-router.use("/user", userRoutes)
-
-
-
 router.use("/assets", AssetUploadRoutes);
 router.use("/adminUser",adminUserRoutes);
-
-
-
-
-
-
 
 export default router;
 
