3738900b4297c18a98b4a4a319a1a22967a3b218
geniusrisky <akashgenius@AKASHs-MacBook-Pro.local>
2022-12-20 18:06:01 +0530

    space managed

--- api/private/v1/index.js
+++ api/private/v1/index.js
@@ -10,19 +10,20 @@ import venueReviewRoutes from "./VenueReviewModule/VenueReview.router.js";
 import venuePhotoRoutes from "./VenuePhotosModule/VenuePhoto.router.js";
 import venueSlotsRoutes from "./VenueSlotsModule/VenueSlots.router.js";
 import venueBookingRoutes from "./VenueBookingModule/VenueBooking.router.js";
-
+import venuePackageRoutes from "./VenuePackageModule/VenuePackage.router.js"
 
 const router = Router();
 
 router.use("/user", userRoutes)
-router.use("/organizations",organizationServiceRoutes)
-router.use("/userSearch",userSearchRoutes)
-router.use("/venue",venueRoutes)
-router.use("/venuePhoto",venuePhotoRoutes)
-router.use("/venueMenu",venueMenuRoutes)
-router.use("/venueReview",venueReviewRoutes)
-router.use("/venueSlots",venueSlotsRoutes)
-router.use("/venueBooking",venueBookingRoutes)
+router.use("/organizations", organizationServiceRoutes)
+router.use("/userSearch", userSearchRoutes)
+router.use("/venue", venueRoutes)
+router.use("/venuePhoto", venuePhotoRoutes)
+router.use("/venueMenu", venueMenuRoutes)
+router.use("/venueReview", venueReviewRoutes)
+router.use("/venueSlots", venueSlotsRoutes)
+router.use("/venueBooking", venueBookingRoutes)
+router.use("/venuePackage", venuePackageRoutes)
 
 export default router;
 
