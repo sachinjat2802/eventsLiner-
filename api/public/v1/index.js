2ea10f540fe1b657756a31d0a82eec41ed6849ea
sachin jat <53407215+sachinjat2802@users.noreply.github.com>
2022-12-26 11:50:44 +0530

    changes in module name

--- api/public/v1/index.js
+++ api/public/v1/index.js
@@ -1,7 +1,7 @@
 import { Router } from "express";
 import AssetUploadRoutes from "./AssetModule/Assets.router.js";
 import adminUserRoutes from "./AdminsUserModule/AdminUserAuth.router.js";
-import userRoutes from "./UserModule/User.route.js"
+import userRoutes from "./UsersModule/User.route.js"
 import venueRoutes from "./VenueModule/Venue.router.js"
 import addOnSellerRoutes from "./AddOnSellerModule/AddOnSeller.router.js";
 
