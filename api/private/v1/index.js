a76d3e91fb281ab841cefa94ce9a4a34c33e486a
sachin jat <53407215+sachinjat2802@users.noreply.github.com>
2022-12-26 12:03:09 +0530

    changes in module name

--- api/private/v1/index.js
+++ api/private/v1/index.js
@@ -18,7 +18,7 @@ import addOnSellerRoutes from "./AddOnSellerModule/AddOnSeller.router.js";
 import addOnSellerPhotosRoutes from "./AddOnSellerPhotosModule/AddOnSellerPhotos.router.js";
 import addOnSellerProductPhotoRoutes from "./AddOnSellerProductPhotosModule/AddOnSellerProductPhotos.router.js"
 import addOnSellerProductRoutes from "./AddOnSellersProductModule/AddOnSellerProduct.router.js"
-import addOnSellerReviewRoutes from "./AddOnSellerReviewModule/AddOnSellerReview.router.js"
+import addOnSellerReviewRoutes from "./AddOnSellerReviewModule/addOnSellerReview.router.js"
 import addOnProductOrderRoutes from "./AddOnProductOrderModule/AddOnProductOrder.router.js"
 import addOnProductCartRoutes from "./AddOnProductCartModule/AddOnProductCart.router.js";
 import eventManagerRoutes from "./EventManagerModule/EventManager.router.js"
