a940f860498e630de30e50d025dca005fe3566aa
sachin jat <53407215+sachinjat2802@users.noreply.github.com>
2022-12-26 10:43:51 +0530

    change in module

--- api/public/v1/index.js
+++ api/public/v1/index.js
@@ -2,14 +2,14 @@ import { Router } from "express";
 import AssetUploadRoutes from "./AssetsModule/assets.router.js";
 import adminUserRoutes from "./AdminUserModule/adminUserAuth.router.js";
 import userRoutes from "./userModule/user.route.js"
-//import venueRoutes from "./venueModule/Venue.router.js"
-import addOnSellerRoutes from "./addOnSellerModule/AddOnSeller.router.js";
+import venueRoutes from "./VenueModule/Venue.router.js"
+import addOnSellerRoutes from "./AddOnSellerModule/AddOnSeller.router.js";
 
 const router = Router();
 router.use("/assets", AssetUploadRoutes);
 router.use("/adminUser",adminUserRoutes);
 router.use("/user",userRoutes);
-//router.use("/venues",venueRoutes);
+router.use("/venues",venueRoutes);
 router.use("/addOnSeller", addOnSellerRoutes)
 
 
