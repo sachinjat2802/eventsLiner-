1a449e22941f26a2d1159d0030570341cf4f00db
sachin jat <53407215+sachinjat2802@users.noreply.github.com>
2022-12-26 10:50:59 +0530

    change in module

--- api/private/v1/index.js
+++ api/private/v1/index.js
@@ -4,7 +4,7 @@ import { Router } from "express";
 import userRoutes from "./userModule/user.router.js";
 import userSearchRoutes from "./userSearchModule/user.router.js";
 import organizationServiceRoutes from "./OrganizationModule/Organization.router.js";
-import venueRoutes from "./venueModule/venue.router.js";
+import venueRoutes from "./venueModule/Venue.router.js";
 import venueMenuRoutes from "./VenueMenuModule/VenueMenu.router.js"
 import venueReviewRoutes from "./VenueReviewModule/VenueReview.router.js";
 import venuePhotoRoutes from "./VenuePhotosModule/VenuePhoto.router.js";
